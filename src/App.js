aaabf75c78ec62fabde1d7eb37f0d275e2b6b416
Wellington Teles <wellingtonteles@gmail.com>
2024-10-14 05:40:32 -0300

    third

--- src/App.js
+++ src/App.js
@@ -5,25 +5,10 @@ import Home from "./pages/Home";
 import Token from "./pages/token";
 import AboutUs from "./pages/aboutus";
 import Intelligence from "./pages/Intelligence";
-import { Helmet } from "react-helmet";
-// import { preview } from "./assets/images/preview.png";
-import Preview from "./assets/images/preview.png";
 
 function App() {
   return (
     <div className="App">
-      <Helmet>
-        <title>DJI</title>
-        <meta property="og:title" content="DJI" />
-        <meta property="og:description" content="Degen Jpegs Incorporated" />
-        <meta property="og:image" content={Preview} />
-        <meta property="og:url" content="https://example.com/your-page" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta
-          name="twitter:image"
-          content="https://example.com/path/to/your/image.jpg"
-        />
-      </Helmet>
       <Router>
         <Routes>
           <Route path="*" element={<Home />} />
